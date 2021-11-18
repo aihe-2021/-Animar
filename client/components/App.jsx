9026fe64cddd1971b7354ada9e33a5806cdc1ac6
Danny <Danny@Apples-iMac.local>
2021-11-19 12:09:19 +1300

    cursors working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,49 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
 
 // import { fetchFruits } from '../actions'
 import { Affirmation } from './Affirmation'
 
+const cursors = [
+  'cursor_01.png',
+  'cursor_02.png',
+  'cursor_03.png',
+  'cursor_04.png',
+  'cursor_05.png'
+]
+
 function App () {
-  const [cursor, setCursor] = useState(['cursor_01.png'])
+  const [cursor, setCursor] = useState('cursor_01.png')
 
-  function handleCursor () {
-    const cursor =
-    [
-      'cursor_01.png',
-      'cursor_02.png',
-      'cursor_03.png',
-      'cursor_04.png',
-      'cursor_05.png'
-    ]
-    setCursor(cursor)
+  function handleCursor (newCursor) {
+    setCursor(newCursor)
+    document.body.style.cursor = `url('/images/${newCursor}'), pointer`
   }
-
-  // const fruits = useSelector(state => state.fruits)
-  // const dispatch = useDispatch()
-
-  // useEffect(() => {
-  //   dispatch(fetchFruits())
-  // }, [])
+  console.log(cursor)
 
   return (<>
-    <div className='app' style={cursor}>
+    <div className='app'>
       <h1>Animar Presents: EDA Monopoly</h1>
+      <h2>Pick Your Playing Piece</h2>
 
-      <button onClick={() => handleCursor(cursor[0])}><img src="./images/cursor_01.png"/></button>
-      <button onClick={() => handleCursor(cursor[1])}><img src="./images/cursor_02.png"/></button>
-      <button onClick={() => handleCursor(cursor[2])}><img src="./images/cursor_03.png"/></button>
-      <button onClick={() => handleCursor(cursor[3])}><img src="./images/cursor_04.png"/></button>
-      <button onClick={() => handleCursor(cursor[4])}><img src="./images/cursor_05.png"/></button>
-
-      {/* <ul>
-          {fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul> */}
+      <button onClick={() => handleCursor(cursors[0])}><img src="./images/cursor_01.png"/></button>
+      <button onClick={() => handleCursor(cursors[1])}><img src="./images/cursor_02.png"/></button>
+      <button onClick={() => handleCursor(cursors[2])}><img src="./images/cursor_03.png"/></button>
+      <button onClick={() => handleCursor(cursors[3])}><img src="./images/cursor_04.png"/></button>
+      <button onClick={() => handleCursor(cursors[4])}><img src="./images/cursor_05.png"/></button>
     </div>
     <div>
       <Affirmation />
+    </div>
+    <div className="board">
     </div>
   </>
   )
